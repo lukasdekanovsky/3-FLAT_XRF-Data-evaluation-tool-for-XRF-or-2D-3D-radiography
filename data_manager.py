69d4e6f654436de3219f26769bed7e74bf43b31f
Lukáš Děkanovský <Lukas.Dekanovsky@advacam.cz>
2024-06-18 17:23:59 +0200

    v1.10 data load/delete + root upload funtional

--- data_manager.py
+++ data_manager.py
@@ -1,15 +1,91 @@
-
+from tkinter import filedialog
+import tkinter as tk
+import shutil
+import datetime
+import os
 
 class DataManager():
 
-    def __init__(self):
-        self.file_path = ""
+    def __init__(self, root):
+        self.data_a_path = "./data_a/"
+        self.data_b_path = "./data_b/"
+        self.results_path = "./results/"
+        self.root = root
+
+    # --------------------------------------------------------------------#
+    # --------------------- DATA LOAD/DELETE------------------------------#
+    # --------------------------------------------------------------------#
+
+    def update_listbox(self, loaded_listbox, path):
+        # UPDATE LISTBOX FUNCTION
+        loaded_listbox.delete(0, tk.END)
+        for file_name in os.listdir(path):
+            loaded_listbox.insert(tk.END, file_name)
+        loaded_listbox.update()
 
     
+    def load_data_a(self, loaded_data_a_listbox):
+        print("A Loading data procedure started")
+        file_paths = filedialog.askopenfilenames(initialdir = "/", title = "Select data A folder", filetypes=(("Text files", "*.txt"), ("all files", "*.*")))
+        if file_paths: # if user selected a folder
+            destination_path = self.data_a_path
+            for file_path in file_paths:
+                file_name = file_path.split("/")[-1]
+                shutil.copy(file_path, destination_path + file_name) 
+            
+            # UPDATE LISTBOX FUNCTION
+            self.update_listbox(loaded_data_a_listbox, self.data_a_path)
 
+    def delete_data_a(self, loaded_data_a_listbox):
+        print("A Deleting data procedure started")
+        file_list = os.listdir(self.data_a_path)
+        for file_name in file_list:
+            file_path = os.path.join(self.data_a_path, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
 
-    def load_data(self):
-        print("Loading data procedure started")
+        # UPDATE LISTBOX FUNCTION
+        self.update_listbox(loaded_data_a_listbox, self.data_a_path)
 
-    def delete_data(self):
-        print("Deleting data procedure started")
+    def load_data_b(self, loaded_data_b_listbox):
+        print("B Loading data procedure started")
+        file_paths = filedialog.askopenfilenames(initialdir = "/", title = "Select data A folder", filetypes=(("Text files", "*.txt"), ("all files", "*.*")))
+        if file_paths: # if user selected a folder
+            destination_path = self.data_b_path
+            for file_path in file_paths:
+                file_name = file_path.split("/")[-1]
+                shutil.copy(file_path, destination_path + file_name)
+            
+            # UPDATE LISTBOX FUNCTION
+            self.update_listbox(loaded_data_b_listbox, self.data_b_path)
+
+    def delete_data_b(self, loaded_data_b_listbox):
+        print("B Deleting data procedure started")
+        file_list = os.listdir(self.data_b_path)
+        for file_name in file_list:
+            file_path = os.path.join(self.data_b_path, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        
+        # UPDATE LISTBOX FUNCTION
+        self.update_listbox(loaded_data_b_listbox, self.data_a_path)
+
+    # --------------------------------------------------------------------#
+    # ---------------------- DATA PROCESSING------------------------------#
+    # --------------------------------------------------------------------#
+
+    def process_data(self, selection_2D_image, selection_2D_CT_scan, selection_FF, selection_4):
+        # DATA processing according to selection
+        processes = [selection_2D_image, selection_2D_CT_scan, selection_FF, selection_4]
+        if selection_2D_image.get():
+            print("Processing 2D image")
+            self.proces_2D_image()
+        elif selection_2D_CT_scan.get():
+            ...
+        elif selection_FF.get():
+            ...
+        elif selection_4.get():
+            ...
+        
+    def proces_2D_image(self):
+        ...
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import tkinter as tk
 from tkinter import *
+from tkinter import filedialog
+import os
 
 
 from data_manager import DataManager
@@ -15,13 +17,28 @@ PROCESSING_FRAME_BACKGROUND = "#E0EEEE"
 DATA_FRAME_BACKGROUND = "#E0EEEE"
 IMAGE_FRAME_BACKGROUND = "#E0EEEE"
 EXPORT_FRAME_BACKGROUND = "#E0EEEE"
+PROCESS_BUTTON_COLOR = "#FFB6C1"
+PROCESS_BUTTON_COLOR = "#FFB6C1"
+REINIT_BUTTON_COLOR = "#FFEC8B"
 
 LOGO_FONT = ('Helvetica', 20, "bold")
 FRAME_LABELS = ('Helvetica', 10, "bold")
 AUTHOR_LABEL = ('Helvetica', 8, "italic")
 
 FRAME_SUBLABEL_1 = ('Helvetica', 10)
-BUTTON_LABEL = ('Helvetica', 10)
+BUTTON_LABEL_A = ('Helvetica', 8)
+BUTTON_LABEL_B = ('Helvetica', 9)
+BUTTON_LABEL_B = ('Helvetica', 9)
+
+# ----------------------------------------------#
+# ----------------- FUNCTIONS-------------------#
+# ----------------------------------------------#
+def reinitialize(root):
+    root.destroy()
+    main()
+
+
+
 
 # ----------------------------------------------#
 # ----------------- UI-SETUP--------------------#
@@ -33,30 +50,30 @@ def main():
     root.title("FLAT_XRF")              # set title of the window
     root.maxsize(850, 850)              # set maximum size of the window
     root.config(bg=ROOT_BACKGROUND)     # set background color of the window
-    
+
 
     # ------------------------#
     # ----- FRAMES SETUP -----#
     # ------------------------#
     # data processing selection frame with bools for processing steps
-    processing_frame = Frame(root, width=300, height=100, bg=PROCESSING_FRAME_BACKGROUND, highlightbackground="black", highlightthickness=1)  
-    processing_frame.grid(row=1, column=0, padx=10, pady=5) 
-    
+    processing_frame = Frame(root, width=300, height=100, bg=PROCESSING_FRAME_BACKGROUND, highlightbackground="black", highlightthickness=1)
+    processing_frame.grid(row=1, column=0, padx=10, pady=5)
+
     # data frame for data load, delete and run the visualization
-    data_frame = Frame(root, width=300, height=560, bg=DATA_FRAME_BACKGROUND, highlightbackground="black", highlightthickness=1)  
-    data_frame.grid(row=2, column=0, rowspan=3, padx=10, pady=5)                          
+    data_frame = Frame(root, width=300, height=560, bg=DATA_FRAME_BACKGROUND, highlightbackground="black", highlightthickness=1)
+    data_frame.grid(row=2, column=0, rowspan=3, padx=10, pady=5)
 
     # logo frame for the SW name and image
-    logo_frame = Frame(root, width=500, height=100, bg=LOGO_FRAME_BACKGROUND)   
+    logo_frame = Frame(root, width=500, height=100, bg=LOGO_FRAME_BACKGROUND)
     logo_frame.grid(row=1, column=1, padx=10, pady=5)
 
     # image frame - main visualization window
-    image_frame = Frame(root, width=500, height=500, bg=IMAGE_FRAME_BACKGROUND, highlightbackground="black", highlightthickness=2)  
-    image_frame.grid(row=2, column=1, padx=10, pady=5)                
+    image_frame = Frame(root, width=500, height=500, bg=IMAGE_FRAME_BACKGROUND, highlightbackground="black", highlightthickness=2)
+    image_frame.grid(row=2, column=1, padx=10, pady=5)
 
-    # export fram - options for image save or other postprocessing? 
-    export_frame = Frame(root, width=500, height=50, bg=EXPORT_FRAME_BACKGROUND, highlightbackground="black", highlightthickness=1)  
-    export_frame.grid(row=3, column=1, padx=10, pady=5)                            
+    # export fram - options for image save or other postprocessing?
+    export_frame = Frame(root, width=500, height=50, bg=EXPORT_FRAME_BACKGROUND, highlightbackground="black", highlightthickness=1)
+    export_frame.grid(row=3, column=1, padx=10, pady=5)
 
     Label(logo_frame, text="FLAT_XRF", font=LOGO_FONT, bg=LOGO_FRAME_BACKGROUND).place(relx=0.15, rely=0.20, anchor=CENTER)
     Label(logo_frame, text="lukasdekanovsky@gmail.com", font=AUTHOR_LABEL, bg=LOGO_FRAME_BACKGROUND).place(relx=0.84, rely=0.85, anchor=CENTER)
@@ -79,42 +96,79 @@ def main():
     Checkbutton(processing_frame, text="Flat field", variable=selection_FF, onvalue=True, offvalue=False).place(relx=0.6, rely=0.4, anchor=W)
     Checkbutton(processing_frame, text="Option 4", variable=selection_4, onvalue=True, offvalue=False).place(relx=0.6, rely=0.8, anchor=W)
     # ---> selection_2D_image.get()  ZISKAVAME TRUE/FALSE HODNOTU Z CHEKCBOXU
-    
+
+
     # ----------------------------#
     # ---------- CLASSES ---------#
     # ----------------------------#
     # class instances as managers
-    data_manager = DataManager()
+    data_manager = DataManager(root)
 
 
 
     # ----------------------------#
     # -------- DATA FRAME --------#
     # ----------------------------#
+
     
-    # LOADED DATA listbox
-    Label(data_frame, text="Loaded data", font=FRAME_SUBLABEL_1, bg=DATA_FRAME_BACKGROUND).place(relx=0.16, rely=0.08, anchor=CENTER)
+    #  -----------------------------LOADED DATA_a listbox------------------------------------------------ #
+    #  -------------------------------------------------------------------------------------------------- #
+    # DATA FOLDERS
+    loaded_data_a_folder = "./data_a"
+    loaded_data_a = os.listdir(loaded_data_a_folder)
+    # LISTBOX
+    Label(data_frame, text="Loaded data A", font=FRAME_SUBLABEL_1, bg=DATA_FRAME_BACKGROUND).place(relx=0.16, rely=0.08, anchor=CENTER)
+    loaded_data_a_scrollbar = tk.Scrollbar(data_frame)
+    loaded_data_a_scrollbar.place(relx=0.45, rely=0.25, anchor=CENTER, relheight=0.3)
+    loaded_data_a_listbox = tk.Listbox(data_frame, yscrollcommand=loaded_data_a_scrollbar.set, width=30, height=10)
+    loaded_data_a_listbox.place(relx=0.215, rely=0.25, width=120, anchor=CENTER)
+    # SHOW ALL FILES in Listbox
+    for data in loaded_data_a:
+        loaded_data_a_listbox.insert(tk.END, data)
 
-    loaded_data_scrollbar = tk.Scrollbar(data_frame)
-    loaded_data_listbox = tk.Listbox(data_frame, yscrollcommand=loaded_data_scrollbar.set, width=30, height=10)
-    loaded_data_listbox.place(relx=0.42, rely=0.25, width=230, anchor=CENTER)
-    loaded_data_scrollbar.place(relx=0.9, rely=0.25, anchor=CENTER, relheight=0.3)
+    # BUTTONS
+    Button(data_frame, text="Load data", font=BUTTON_LABEL_A, command = lambda: data_manager.load_data_a(loaded_data_a_listbox)).place(relx=0.11, rely=0.43, anchor=CENTER)
+    Button(data_frame, text="Delete data", font=BUTTON_LABEL_A, command=lambda: data_manager.delete_data_a(loaded_data_a_listbox)).place(relx=0.33, rely=0.43, anchor=CENTER)
+    Button(data_frame, width=15, text="Process", font=BUTTON_LABEL_B, bg=PROCESS_BUTTON_COLOR, command=lambda: data_manager.process_data(selection_2D_image, selection_2D_CT_scan, selection_FF, selection_4)).place(relx=0.66, rely=0.485, anchor=CENTER)
+    Button(data_frame, width=15, text="Reinitialize window", font=BUTTON_LABEL_B, bg=REINIT_BUTTON_COLOR, command=lambda: reinitialize(root)).place(relx=0.26, rely=0.485, anchor=CENTER)
+   #  -------------------------------------------------------------------------------------------------- #
+   #  -------------------------------------------------------------------------------------------------- #
 
-    Button(data_frame, text="Load data", font=BUTTON_LABEL, command = lambda: data_manager.load_data()).place(relx=0.15, rely=0.44, anchor=CENTER)
-    Button(data_frame, text="Delete data", font=BUTTON_LABEL, command=lambda: data_manager.delete_data()).place(relx=0.41, rely=0.44, anchor=CENTER)
 
-    # PROCESSED data listbox
-    Label(data_frame, text="Processed data", font=FRAME_SUBLABEL_1, bg=DATA_FRAME_BACKGROUND).place(relx=0.19, rely=0.53, anchor=CENTER)
+   #  -----------------------------LOADED DATA_b listbox------------------------------------------------ #
+   #  -------------------------------------------------------------------------------------------------- #
+   # DATA FOLDERS
+    loaded_data_b_folder = "./data_b"
+    loaded_data_b = os.listdir(loaded_data_b_folder)
+    # LISTBOX
+    Label(data_frame, text="Loaded data B", font=FRAME_SUBLABEL_1, bg=DATA_FRAME_BACKGROUND).place(relx=0.65, rely=0.08, anchor=CENTER)
+    loaded_data_b_scrollbar = tk.Scrollbar(data_frame)
+    loaded_data_b_scrollbar.place(relx=0.94, rely=0.25, anchor=CENTER, relheight=0.3)
+    loaded_data_b_listbox = tk.Listbox(data_frame, yscrollcommand=loaded_data_b_scrollbar.set, width=30, height=10)
+    loaded_data_b_listbox.place(relx=0.705, rely=0.25, width=120, anchor=CENTER)
+    # SHOW ALL FILES in Listbox
+    for data in loaded_data_b:
+        loaded_data_b_listbox.insert(tk.END, data)
+    # BUTTONS
+    Button(data_frame, text="Load data", font=BUTTON_LABEL_A, command = lambda: data_manager.load_data_b(loaded_data_b_listbox)).place(relx=0.60, rely=0.43, anchor=CENTER)
+    Button(data_frame, text="Delete data", font=BUTTON_LABEL_A, command=lambda: data_manager.delete_data_b(loaded_data_b_listbox)).place(relx=0.82, rely=0.43, anchor=CENTER)
+    #  -------------------------------------------------------------------------------------------------- #
+    #  -------------------------------------------------------------------------------------------------- #
+
+
+    #  -----------------------------PROCESSED DATA listbox----------------------------------------------- #
+    #  -------------------------------------------------------------------------------------------------- #
+    Label(data_frame, text="Processed data", font=FRAME_SUBLABEL_1, bg=DATA_FRAME_BACKGROUND).place(relx=0.19, rely=0.57, anchor=CENTER)
 
     processed_data_scrollbar = tk.Scrollbar(data_frame)
-    processed_data_listbox = tk.Listbox(data_frame, yscrollcommand=loaded_data_scrollbar.set, width=30, height=10)
-    processed_data_listbox.place(relx=0.42, rely=0.7, width=230, anchor=CENTER)
-    processed_data_scrollbar.place(relx=0.9, rely=0.7, anchor=CENTER, relheight=0.3)
+    processed_data_scrollbar.place(relx=0.9, rely=0.75, anchor=CENTER, relheight=0.3)
+    processed_data_listbox = tk.Listbox(data_frame, yscrollcommand=processed_data_scrollbar.set, width=30, height=10)
+    processed_data_listbox.place(relx=0.42, rely=0.75, width=230, anchor=CENTER)
+    #  -------------------------------------------------------------------------------------------------- #
 
 
 
 
-    root.update()                       # update the window continuously
     root.mainloop()                     # start the main loop of the window
 
 
